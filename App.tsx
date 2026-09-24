ad746d853514df42e981a9e09f6323f56fd5d679
Tejasvita <ttejasvita@gmail.com>
2026-09-24 13:39:39 +0530

    understanding react native components

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 
 function App() {
   return (
     <View>
       <Text style={{ fontSize: 30 }}>Hello from react native</Text>
+      <Button title="Press me"></Button>
+      <Button title="Press me too"></Button>
     </View>
   );
 }
