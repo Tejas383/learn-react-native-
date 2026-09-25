ce1c54fe9afebc40e9ab56da8c053fab5c54da77
Tejasvita <ttejasvita@gmail.com>
2026-09-25 11:59:27 +0530

    components

--- App.tsx
+++ App.tsx
@@ -22,8 +22,25 @@ function App() {
       <Text style={{ fontSize: 15 }}>
         {age > 18 ? 'adult' : 'anauthorised'}
       </Text>
+
+      <Text style={{ fontSize: 30 }}>Components</Text>
+      <UserData />
     </View>
   );
 }
+
+// we can have multiple components in a file
+const UserData = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 15 }}>Name : Tejasvita</Text>
+      <Text style={{ fontSize: 15 }}>Age : 21</Text>
+      <Text style={{ fontSize: 15 }}>Gender : Female</Text>
+    </View>
+  );
+};
+// components can be nested
+
+// but, it is better to keep components inside a components folder
 
 export default App;
