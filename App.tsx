e939b4ed5b79f5e0884cab43db0221a44e5db4fc
Tejasvita <ttejasvita@gmail.com>
2026-09-25 11:51:59 +0530

    rendering variables

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import { Button, Text, View } from 'react-native';
 
 function App() {
+  const name = 'Tejasvita';
+  var age = 21;
+  let email = 'tejasvita@gmail.com';
+
+  const fruit = () => {
+    return 'apple';
+  };
+
   return (
     <View>
       <Text style={{ fontSize: 30 }}>Hello from react native</Text>
       <Button title="Press me"></Button>
       <Button title="Press me too"></Button>
+      <Text style={{ fontSize: 15 }}>{name}</Text>
+      <Text style={{ fontSize: 15 }}>{age}</Text>
+      <Text style={{ fontSize: 15 }}>{email}</Text>
+      <Text style={{ fontSize: 15 }}>{fruit()}</Text>
+      <Text style={{ fontSize: 15 }}>{10 * 50}</Text>
+      <Text style={{ fontSize: 15 }}>
+        {age > 18 ? 'adult' : 'anauthorised'}
+      </Text>
     </View>
   );
 }
